7a145a34b249fb3197b836550d6d86699403b0ce
Eunhyung Choi <milkiss0414@naver.com>
2022-05-11 12:42:56 +0900

    Revert "Feat: Calendar 모듈 추가"
    This reverts commit 8d0f1b9c372e20d1e063e390188b063ebccecf66.

--- App.js
+++ App.js
@@ -3,8 +3,7 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import VoteList from './src/screens/vote/VoteList.js';
-import MainPage from './src/screens/main/MainPage.js';
+import VoteList from './src/screens/vote/voteList.js';
 
 
 const Stack = createNativeStackNavigator();
@@ -13,7 +12,6 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="VoteList">
-        <Stack.Screen name="mainPage" component={MainPage} options={{headerShown: false}}/>
         <Stack.Screen name="VoteList" component={VoteList} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
