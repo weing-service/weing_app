35ad66c6a590202e6bb50293314576c97e838802
Eunhyung Choi <milkiss0414@naver.com>
2022-05-13 02:09:48 +0900

    Update: projectBar 및 topBar 수정

--- src/components/main/topBar.js
+++ src/components/main/topBar.js
@@ -1,10 +1,18 @@
 // 공유, 알림, 프로필 버튼을 포함하는 상단바 컴포넌트
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 
 const TopBar = () => {
-  return <View>
-    <Text>알림창</Text>
+  return <View style={{flexDirection: 'row-reverse'}}>
+  <TouchableOpacity>
+    <Image source={require('../../assets/main/top/btn_profile.png')}/>
+  </TouchableOpacity>
+    <TouchableOpacity>
+      <Image source={require('../../assets/main/top/btn_alert.png')}/>
+    </TouchableOpacity>
+    <TouchableOpacity>
+      <Image source={require('../../assets/main/top/btn_share.png')}/>
+    </TouchableOpacity>
   </View>;
 };
 
