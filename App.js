7863c9c7d81e42716edcb91da979959a21c3a912
Hwangseungwoo <simodda@yonsei.ac.kr>
2022-05-22 12:58:03 +0900

    Feat: 탭 네비게이터 작업

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import VoteList from './src/screens/vote/voteList';
 import VoteResult from './src/screens/vote/voteResult';
@@ -12,13 +13,26 @@ import Login from './src/screens/login/login';
 import MainPage from './src/screens/main/mainPage';
 import AddTodoPage from './src/screens/main/addTodoPage';
 import AddProjectPage from './src/screens/main/AddProjectPage';
+import Map from './src/screens/map/map';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <NavigationContainer>
+        <Tab.Navigator
+            initialRouteName = "MainPage">
+          <Tab.Screen name="VoteList" component={VoteList} options={{ headerShown: false, tabBarShowLabel : false }} />
+          <Tab.Screen name="MainPage" component={MainPage} options={{ headerShown: false, tabBarShowLabel : false }} />
+          <Tab.Screen name="Map" component={Map} options={{ headerShown: false, tabBarShowLabel : false }} />
+        </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
 
 function App() {
   return (
-
-
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Loading">
         <Stack.Screen name="Loading" component={Loading} options={{headerShown: false}}/>
@@ -31,6 +45,8 @@ function App() {
         <Stack.Screen name="AddProjectPage" component={AddProjectPage} options={{headerShown: false}}/>
         <Stack.Screen name="MainPage" component={MainPage} options={{headerShown: false}}/>
         <Stack.Screen name="AddTodoPage" component={AddTodoPage} options={{headerShown: false}}/>
+        <Stack.Screen name="Map" component={Map} options={{headerShown: false}}/>
+        <Stack.Screen name="MyTabs" component={MyTabs} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
