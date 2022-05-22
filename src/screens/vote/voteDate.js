d912d5d36d6c800598f1fb693b20d6816c0dd2f4
Hwangseungwoo <simodda@yonsei.ac.kr>
2022-05-22 22:58:07 +0900

    Feat: 날짜 선택 창 진행 1차

--- src/screens/vote/voteDate.js
+++ src/screens/vote/voteDate.js
@@ -6,11 +6,48 @@ import { useNavigation } from '@react-navigation/native';
 const VoteDo = () => {
 
     const navigation = useNavigation();
+
+    const data = ['2022-05-15','2022-05-16','2022-05-17','2022-05-18','2022-05-19','2022-05-20','2022-05-21','2022-05-22'];
+
+    const mapToComponent = () => {
+          return data.map((element) => {
+            return (
+                <VoteCard
+                    key = {element.key} data = {element} style = {projectInfo[props.state]}/>
+            );
+          });
+        };
+
+
     return (
         <View>
             <ImageBackground
                 style = {{width: '100%', height: '100%'}}
                 source={require('../../assets/background/vote.png')}>
+                <View
+                    style = {{width : '100%', height : 264, backgroundColor : 'white', borderBottomLeftRadius : 10, borderBottomRightRadius : 10}}>
+                    <View
+                        style = {{flexDirection: 'row', justifyContent : 'space-between'}}>
+                        <Text
+                            style = {styles.title}>
+                            날짜/시간 선택
+                        </Text>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('VoteDo')}>
+                            <Text
+                                style = {styles.title2}>
+                                완료
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View
+                        style = {{width : '90%', borderTopWidth : 1, alignSelf : 'center', borderColor : '#DBDBDB'}}>
+                    </View>
+                    <Text
+                        style = {{color : '#404855', marginLeft : 16, marginTop : 12}}>
+                        날짜 선택
+                    </Text>
+                </View>
                 <TouchableOpacity
                     onPress={() => navigation.navigate('VoteDo')}>
                     <Text
@@ -22,5 +59,24 @@ const VoteDo = () => {
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color : 'black',
+        marginTop: 34,
+        marginBottom : 28,
+        marginLeft : '32%'
+    },
+    title2: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        color : '#999999',
+        marginTop: 35,
+        marginBottom : 4,
+        marginRight : '4%'
+    },
+});
 
 export default VoteDo;
