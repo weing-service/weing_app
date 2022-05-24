a0cfdd4ba18ea12dd255f858cd1c383ca118d0b4
Hwangseungwoo <simodda@yonsei.ac.kr>
2022-05-25 01:02:05 +0900

    Feat: 시간 선택 뷰 추가

--- src/components/vote/timeTouchView.js
+++ src/components/vote/timeTouchView.js
@@ -4,16 +4,28 @@ import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 
 const TimeTouchView  = (props) => {
 
-    const newData = [...props.data, ...props.data];
-    newData.pop();
-    newData.pop();
+    const array = [];
+    for (let i=0; i<30;i++){
+        array.push(i)
+    }
+    const [ time, setTime ] = useState([]);
 
     const mapToComponent = () => {
-        return newData.map((element) => {
+        return array.map((element) => {
+
             return (
                 <TouchableOpacity
-                    style = {{width : 70, height : 24, marginLeft : 52, backgroundColor : 'white', borderWidth : 1, borderColor : '#89B6C2'}}
-                    key = {element.index}/>
+                    style = {{backgroundColor : time.includes(element) ? '#D2DFCB' : 'white',
+                            marginLeft : 52,borderWidth : 1, borderColor : '#89B6C2', width : 70, height : 24,}}
+                    key = {element}
+                    onPress = {() => {
+                        if (time.includes(element)){
+                            setTime(time.filter((x) => x !== element));
+                            console.log(time);
+                        } else {
+                            setTime([...time, element]);
+                            console.log(time);
+                        }}}/>
             );
         })
     }
